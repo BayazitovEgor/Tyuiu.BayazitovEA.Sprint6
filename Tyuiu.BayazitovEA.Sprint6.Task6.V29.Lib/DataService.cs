132cd29495462dbe0e4b713b7d8079410cde5f97
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-12-10 03:04:31 +0500

    6

--- Tyuiu.BayazitovEA.Sprint6.Task6.V29.Lib/DataService.cs
+++ Tyuiu.BayazitovEA.Sprint6.Task6.V29.Lib/DataService.cs
@@ -1,24 +1,39 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint6;
+using System.IO;
+
 namespace Tyuiu.BayazitovEA.Sprint6.Task6.V29.Lib
 {
     public class DataService : ISprint6Task6V29
     {
         public string CollectTextFromFile(string path)
         {
-            string str = "i";
-            string resStr = "";
+            string result = "";
+
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    if (line.Contains(str))
+                    // Split line into words
+                    string[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string word in words)
                     {
-                        resStr = resStr + " " + line;
+                        // Check if word contains lowercase 'i'
+                        if (word.Contains("i"))
+                        {
+                            // Add to result with space if not first word
+                            if (!string.IsNullOrEmpty(result))
+                            {
+                                result += " ";
+                            }
+                            result += word;
+                        }
                     }
                 }
             }
-            return resStr;
+
+            return result;
         }
     }
 }
